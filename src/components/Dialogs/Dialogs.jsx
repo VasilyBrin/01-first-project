80b9ecc4ffc602b941c8cde0126feb9ccc12685e
VasilyBrin <mail@profitdata.ru>
2021-05-16 16:46:48 +0300

    refresh Dialogs.jsx

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,33 +1,36 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import s from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return (
+        <BrowserRouter>
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
                 <div className={s.dialog + ' ' + s.active}>
-                    Basil
+                    <NavLink to="/dialogs/1">Basil</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Natan
+                    <NavLink to="/dialogs/2">Natan</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Sergey
+                    <NavLink to="/dialogs/3">Sergey</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Ilya
+                    <NavLink to="/dialogs/4">Ilya</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Kostya
+                    <NavLink to="/dialogs/5">Kostya</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Nastya
+                    <NavLink to="/dialogs/6">Nastya</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Nika
+                    <NavLink to="/dialogs/7">Nika</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Eva
+                    <NavLink to="/dialogs/8">Eva</NavLink>
                 </div>
             </div>
             <div className={s.messages}>
@@ -36,6 +39,7 @@ const Dialogs = (props) => {
                 <div className={s.dialog}>Yo</div>
             </div>
         </div>
+        </BrowserRouter>
     )
 }
 
