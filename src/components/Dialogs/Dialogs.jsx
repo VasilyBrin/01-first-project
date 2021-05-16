95cc38ebd404d8cd4917f074f0d327db3a7d783f
VasilyBrin <mail@profitdata.ru>
2021-05-16 10:50:34 +0300

    Clone pages from 01-first project

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,38 @@ import s from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Basil
+                </div>
+                <div className={s.dialog}>
+                    Natan
+                </div>
+                <div className={s.dialog}>
+                    Sergey
+                </div>
+                <div className={s.dialog}>
+                    Ilya
+                </div>
+                <div className={s.dialog}>
+                    Kostya
+                </div>
+                <div className={s.dialog}>
+                    Nastya
+                </div>
+                <div className={s.dialog}>
+                    Nika
+                </div>
+                <div className={s.dialog}>
+                    Eva
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.dialog}>Hi</div>
+                <div className={s.dialog}>How is your command?</div>
+                <div className={s.dialog}>Yo</div>
+            </div>
         </div>
     )
 }
